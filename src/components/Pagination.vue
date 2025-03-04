f7fe605c45937505aa1ca05077d72383d04cbc5a
wanggao <2556812561@qq.com>
2025-03-04 11:29:44 +0800

    分页组件去掉total

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -6,7 +6,7 @@
       :page-sizes="[10, 20, 30, 50]"
       :small="small"
       :background="true"
-      layout="total, sizes, prev, pager, next, jumper"
+      layout="sizes, prev, pager, next, jumper"
       :total="total"
       @update:current-page="$emit('update:currentPage', $event)"
       @update:page-size="$emit('update:pageSize', $event)"
@@ -21,10 +21,15 @@ defineProps({
   currentPage: { type: Number, default: 1 },
   pageSize: { type: Number, default: 10 },
   total: { type: Number, default: 0 },
-  small: { type: Boolean, default: false }
+  small: { type: Boolean, default: false },
 })
 
-defineEmits(['update:currentPage', 'update:pageSize', 'size-change', 'current-change'])
+defineEmits([
+  'update:currentPage',
+  'update:pageSize',
+  'size-change',
+  'current-change',
+])
 </script>
 
 <style scoped>
@@ -33,5 +38,23 @@ defineEmits(['update:currentPage', 'update:pageSize', 'size-change', 'current-ch
   justify-content: flex-end;
   margin-top: 20px;
   padding-right: 20px;
+}
+
+:deep(.el-pagination) {
+  --el-pagination-bg-color: #f4f4f5;
+  --el-pagination-hover-color: #409eff;
+}
+
+:deep(.el-pagination.is-background .el-pager li:not(.is-disabled)) {
+  background-color: var(--el-pagination-bg-color);
+}
+
+:deep(.el-pagination.is-background .el-pager li:not(.is-disabled).is-active) {
+  background-color: var(--el-pagination-hover-color);
+}
+
+:deep(.el-pagination.is-background .btn-prev),
+:deep(.el-pagination.is-background .btn-next) {
+  background-color: var(--el-pagination-bg-color);
 }
 </style>
